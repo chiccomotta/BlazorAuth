30f1458d8beac22753dcb44651b7b6751079c629
cristiano motta <cristiano.motta@hotmail.it>
2025-07-15 13:46:56 +0200

    Major renaming

--- BlazorAuthenticationTutorial/Shared/Models/Authentication.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorAuthenticationTutorial.Shared.Models;
-
-public class Authentication
-{
-    public string SecretKey { get; set; }
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
-}
